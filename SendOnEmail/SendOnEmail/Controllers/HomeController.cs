540987c4a0009544489b46a63bd92e91e1b7248d
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2020-05-13 22:10:58 +0300

    Sending on Email ASP.NET Core/ 2 ways to implenment
    1 way - using System.Net.Mail
    2 way - using MailKit package

---
+++ SendOnEmail/SendOnEmail/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SendOnEmail.Models;
+
+namespace SendOnEmail.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly Service service;
+        public HomeController(ILogger<HomeController> logger, Service service)
+        {
+            _logger = logger;
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendEmailDefault()
+        {
+            service.SendEmailDefault();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult SendEmailCustom()
+        {
+            service.SendEmailCustom();
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SendOnEmail/SendOnEmail/Service.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Logging;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace SendOnEmail
+{
+    public class Service
+    {
+        private readonly ILogger<Service> logger;
+        
+        public Service(ILogger<Service> logger)
+        {
+            this.logger = logger;
+        }
+        public void SendEmailDefault()// google stoped support System.Net.Mail and recommend using third-party app
+        {
+            try
+            {
+                //System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                MailMessage message = new MailMessage();
+                message.IsBodyHtml = true;
+                message.From = new MailAddress("EventsCo@mycompany.com", "Events Co");
+                message.To.Add("ola190.ger@gmail.com");
+                message.Subject = "Message from Events Co";
+                message.Body = "<div style=\"color:red;\"> Message with your email and pss for sign on web page</div>";
+                //message.Attachments.Add("... path to the file ...");
+
+                using (SmtpClient client = new SmtpClient("smtp.gmail.com"))
+                {
+                    client.Credentials = new NetworkCredential("Forworkchange@gmail.com", "Abrakadabra2015");
+                    client.Port = 587;//465;
+                    
+                    client.EnableSsl = true;
+
+                    client.Send(message);
+                    logger.LogInformation("Email sent succesfully");
+                }
+
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.GetBaseException().Message);
+            }
+        }
+
+        public void SendEmailCustom()//with mailkit
+        {
+            try
+            {
+                MimeMessage message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Events Co", "EventsCo@mycompany.com"));
+                message.To.Add(new MailboxAddress("ola190.ger@gmail.com"));
+                message.Subject = "Message from Events Co";
+                message.Body = new BodyBuilder() {HtmlBody= "<div style=\"color:green;\"> Message with your email and pss for sign on web page</div>" }.ToMessageBody();
+
+                using (MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 465, true);
+                    client.Authenticate("Forworkchange@gmail.com", "Abrakadabra2015");
+                    client.Send(message);
+                    client.Disconnect(true);
+                    logger.LogInformation("Email sent succesfully");
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.GetBaseException().Message);
+            }
+        }
+    }
+}
